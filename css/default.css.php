6ce42748aba73333dbb28761dd198d102002c71b
Richterslava <robert.bue@gmail.com>
2012-02-14 14:44:35 +0100

    progress

---
+++ css/default.css.php
@@ -0,0 +1,27 @@
+<?php
+
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
+header('Content-type: text/css');
+
+$showBorders = false;
+
+?>*
+{
+    margin: 0;
+    padding: 0;
+
+    color: #000;
+    font-family: verdana;
+}
+
+img {
+    border: none;
+}
+
+.hide
+{
+    visibility: hidden;
+    width: 100%;
+    clear: both;
+}
